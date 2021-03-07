22228bce71c81fbbc24d9a77a1b96113ebe47a85
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-03-07 17:00:45 -0500

    correct version

--- data_deliverable/data/clean_data.py
+++ data_deliverable/data/clean_data.py
@@ -8,7 +8,7 @@ df = pd.DataFrame(data, columns= ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V
 
 conn = sqlite3.connect('transactions.db')
 cursor = conn.cursor()
-conn.execute('''CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, Time1 INTEGER NOT NULL, V1 FLOAT NOT NULL, V2 FLOAT NOT NULL, V3 FLOAT NOT NULL, V4 FLOAT NOT NULL, V5 FLOAT NOT NULL,
+conn.execute('''CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, Delta_T INTEGER NOT NULL, V1 FLOAT NOT NULL, V2 FLOAT NOT NULL, V3 FLOAT NOT NULL, V4 FLOAT NOT NULL, V5 FLOAT NOT NULL,
                                                         V6 FLOAT NOT NULL, V7 FLOAT NOT NULL, V8 FLOAT NOT NULL, V9 FLOAT NOT NULL, V10 FLOAT NOT NULL, V11 FLOAT NOT NULL, V12 FLOAT NOT NULL, V13 FLOAT NOT NULL,
                                                         V14 FLOAT NOT NULL, V15 FLOAT NOT NULL, V16 FLOAT NOT NULL, V17 FLOAT NOT NULL, V18 FLOAT NOT NULL, V19 FLOAT NOT NULL, V20 FLOAT NOT NULL,
                                                         V21 FLOAT NOT NULL, V22 FLOAT NOT NULL, V23 FLOAT NOT NULL, V24 FLOAT NOT NULL, V25 FLOAT NOT NULL, V26 FLOAT NOT NULL, V27 FLOAT NOT NULL, V28 FLOAT NOT NULL,
@@ -16,7 +16,7 @@ conn.execute('''CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY 
 
 for row in df.itertuples():
     conn.execute('''
-        INSERT INTO transactions (Time1, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17,
+        INSERT INTO transactions (Delta_T, V1, V2, V3, V4, V5, V6, V7, V8, V9, V10, V11, V12, V13, V14, V15, V16, V17,
                                     V18, V19, V20, V21, V22, V23, V24, V25, V26, V27, V28, Amount , Class)
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
         ''',
