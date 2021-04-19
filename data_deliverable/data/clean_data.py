41362c1f97544a47e8bda5df48aaffe1b2d356a8
Haider-Asif <60201762+Haider-Asif@users.noreply.github.com>
2021-04-19 06:58:48 +0500

    push

--- data_deliverable/data/clean_data.py
+++ data_deliverable/data/clean_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sqlite3
 
-data = pd.read_csv ('creditcard.csv')   
+data = pd.read_csv ('./data_deliverable/data/creditcard.csv')   
 df = pd.DataFrame(data, columns= ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 'V11', 
                                     'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20', 'V21', 'V22', 
                                     'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount', 'Class'])
